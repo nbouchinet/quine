ed960b28753b0730031d1b325fd40beb22520375
Nicolas BOUCHINET <nbouchin@e1r6p20.42.fr>
2019-08-14 12:11:49 +0200

    UPDATE(Colleen asm done)

--- ASM/Colleen/Colleen.s
+++ ASM/Colleen/Colleen.s
@@ -1,18 +1,21 @@
-section .text
 extern _printf
+global _main
+section .text
 ;out
 _f:
 	ret
-
-start:
+_main:
 	;in
+	enter 0, 0
 	call _f
 	lea rdi, [rel s]
 	mov rsi, 10
 	mov rdx, 34
 	lea rcx, [rel s]
+	xor rax, rax
 	call _printf
+	leave
 	ret
 
 section .data
-s db "section .text$1%cextern _printf$1%c;out%1$c_f:$1%c	ret%1$c%1$cstart:%1$c	;in%1$c	call _f%1$cmov rdi, s%1$cmov rsi, 10%1$cmov rdx, 34%1$cmov rcx, s%1$ccall _printf%1ret$c%1$csection .bss%1$c s: db %2$c%3$s%2$c"
+s db "extern _printf%1$cglobal _main%1$csection .text%1$c;out%1$c_f:%1$c	ret%1$c_main:%1$c	;in%1$c	enter 0, 0%1$c	call _f%1$c	lea rdi, [rel s]%1$c	mov rsi, 10%1$c	mov rdx, 34%1$c	lea rcx, [rel s]%1$c	xor rax, rax%1$c	call _printf%1$c	leave%1$c	ret%1$c%1$csection .data%1$cs db %2$c%3$s%2$c"
